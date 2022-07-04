b2e1090245f158e7eb231331689d7028404f41e5
hom-bahrani <hab0184@gmail.com>
2022-07-04 11:11:01 +0100

    Add a data bucket

--- molecule_solubility_poc/molecule_solubility_poc_stack.py
+++ molecule_solubility_poc/molecule_solubility_poc_stack.py
@@ -4,6 +4,7 @@ from aws_cdk import (
     aws_ecr as ecr,
     aws_ecs as ecs,
     aws_logs as logs,
+    aws_s3 as s3,
     aws_ecs_patterns as ecs_patterns,
 )
 from constructs import Construct
@@ -12,16 +13,24 @@ class MoleculeSolubilityPocStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        
+        databucket = s3.Bucket(self, 
+                               'molecule_solubility_dataBucket', 
+                               removal_policy=cdk.RemovalPolicy.DESTROY, 
+                               auto_delete_objects=True, 
+                               bucket_name='molecule_solubility_dataBucket')
 
         ecr_repository = ecr.Repository(self, 
-                                        id=' molecule_solubility_ecr_repository',
+                                        id='molecule_solubility_ecr_repository',
                                         repository_name='molecule_solubility_repository')
         
         cluster = ecs.Cluster(self, 
                               'molecule_solubility_EcsCluster')
         
         task_definition = ecs.FargateTaskDefinition(self, 
-                                                    'MoleculeSolubilityDemoServiceTask', 
+                                                    'MoleculeSolubilityDemoServiceTask',
+                                                    cpu=1024,
+                                                    memory_limit_mib=2048, 
                                                     family='MoleculeSolubilityDemoServiceTask')
         
         image = ecs.ContainerImage.from_asset('service')
@@ -44,8 +53,6 @@ class MoleculeSolubilityPocStack(Stack):
                                                            'molecule_solubility_Service',
                                                            cluster=cluster,
                                                            desired_count=1,
-                                                           cpu=1024,
-                                                           memory_limit_mib=2048,
                                                            task_definition=task_definition)
         
         
